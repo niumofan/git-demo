7c64ddd9b07a646b35a9de97b27044c6f60e34b4
nmf <838095271@qq.com>
2022-07-30 16:50:30 +0800

    zhang fix bug

--- src/Book.java
+++ src/Book.java
@@ -1,5 +1,7 @@
 public class Book {
     public static void main(String[] args) {
-        printf("我是小袁");
+        System.out.println("我是小袁");
+
+        System.out.println("我是小张，修改了小袁代码的bug");
     }
 }
